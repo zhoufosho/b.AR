91af245c52d8c199a292be70453aa429acc5c2a8
Jordan <jacazamias@gmail.com>
2016-06-01 10:36:06 -0700

    Add seed transition to tree

---
+++ CoinsForClimate/Assets/plantseed/PlantSeed.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantSeed : MonoBehaviour {
+
+    Animator anim;
+    TreeControl tree;
+
+    bool done = false;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        tree = GameObject.Find("Tree").GetComponent<TreeControl>();
+
+        Debug.Assert(anim != null, "Seed object could not find its own Animator component");
+        Debug.AssertFormat(tree != null, "Seed object could not find GameObject '{0}'", "Tree");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Once animation has completed, begin tree growth stage
+        if (done == false && anim.GetCurrentAnimatorStateInfo(0).IsName("Done"))
+        {
+            done = true;
+            Camera.main.GetComponent<GroundEnemy>().enabled = true;
+            Camera.main.GetComponent<AirEnemy>().enabled = true;
+            Camera.main.GetComponent<ShotScript>().enabled = true;
+
+            tree.enabled = true;
+        }
+    }
+}
