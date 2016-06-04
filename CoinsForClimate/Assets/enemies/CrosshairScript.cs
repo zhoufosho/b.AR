44e00009402cdf98b185974b73cd32437182e9ad
Shannon Kao <skao127@gmail.com>
2016-06-04 12:25:03 -0700

    Crosshairs in enemy scene

---
+++ CoinsForClimate/Assets/enemies/CrosshairScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrosshairScript : MonoBehaviour {
+
+    public Texture2D crosshairTexture;
+    public float crosshairScale = 1;
+     
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnGUI() {
+         //if not paused
+         if(Time.timeScale != 0) {
+             if(crosshairTexture!=null) {
+                 GUI.DrawTexture(new Rect((Screen.width-crosshairTexture.width*crosshairScale)/2 ,(Screen.height-crosshairTexture.height*crosshairScale)/2, crosshairTexture.width*crosshairScale, crosshairTexture.height*crosshairScale),crosshairTexture);
+             } else {
+                 Debug.Log("No crosshair texture set in the Inspector");
+             }
+         }
+    }
+     
+}
+
+
+ 
+     
