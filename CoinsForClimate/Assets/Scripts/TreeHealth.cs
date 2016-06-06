aa9088faa0c1edee116706413980f402310eed50
Jordan <jacazamias@gmail.com>
2016-06-05 19:25:46 -0700

    Add treehealth script, will add grass soon

---
+++ CoinsForClimate/Assets/Scripts/TreeHealth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grass : MonoBehaviour {
+
+    public float grassMaxRadius = 1f;
+    public int grassNumPlants = 100;
+
+    [Range(0f, 1f)]
+    public float growth = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+        // Choose random points to place grass
+        for(int i = 0; i < grassNumPlants; i++)
+        {
+
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
